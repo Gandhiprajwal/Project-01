05375c9afa8699b832a124dc589841a77b799228
Prajwal Gandhi <1000015996@dit.edu.in>
2025-04-02 13:19:43 +0530

    js issue

--- src/js/main.js
+++ src/js/main.js
@@ -20,67 +20,67 @@ fetch("footer.html")
   })
   .catch((err) => console.log(err));
 
-// stock
-fetch("stock.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("stock").innerHTML = data;
-  })
-  .catch((err) => console.log(err));
+// // stock
+// fetch("stock.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("stock").innerHTML = data;
+//   })
+//   .catch((err) => console.log(err));
 
-// financial planning
-fetch("financial_planning.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("financial").innerHTML = data;
-  })
-  .catch((err) => console.log(err));
+// // financial planning
+// fetch("financial_planning.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("financial").innerHTML = data;
+//   })
+//   .catch((err) => console.log(err));
 
-// tax
-fetch("tax.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("tax").innerHTML = data;
-  })
-  .catch((err) => console.log(err));
+// // tax
+// fetch("tax.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("tax").innerHTML = data;
+//   })
+//   .catch((err) => console.log(err));
 
-//  careers
-fetch("careers.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("careers").innerHTML = data;
-  })
-  .catch((err) => console.log(err));
+// //  careers
+// fetch("careers.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("careers").innerHTML = data;
+//   })
+//   .catch((err) => console.log(err));
 
-// why us
-fetch("why-us.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("why-us").innerHTML = data;
-  })
-  .catch((err) => console.log(err));
+// // why us
+// fetch("why-us.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("why-us").innerHTML = data;
+//   })
+//   .catch((err) => console.log(err));
 
-// services
-fetch("services.html")
-  .then((res) => res.text())
-  .then((data) => {
-    document.getElementById("services").innerHTML = data;
-  })
-  .catch((err) => console.log(err));
+// // services
+// fetch("services.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("services").innerHTML = data;
+//   })
+//   .catch((err) => console.log(err));
 
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("news-container");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const container = document.getElementById("news-container");
 
-  fetch("news.html")
-    .then((response) => {
-      if (!response.ok) throw new Error("Network error");
-      return response.text();
-    })
-    .then((data) => {
-      container.insertAdjacentHTML("beforeend", data); // Append the HTML content
-    })
-    .catch((error) => {
-      console.error("Error loading news:", error);
-      container.innerHTML = "<p>Failed to load news content.</p>";
-    });
-});
+//   fetch("news.html")
+//     .then((response) => {
+//       if (!response.ok) throw new Error("Network error");
+//       return response.text();
+//     })
+//     .then((data) => {
+//       container.insertAdjacentHTML("beforeend", data); // Append the HTML content
+//     })
+//     .catch((error) => {
+//       console.error("Error loading news:", error);
+//       container.innerHTML = "<p>Failed to load news content.</p>";
+//     });
+// });
